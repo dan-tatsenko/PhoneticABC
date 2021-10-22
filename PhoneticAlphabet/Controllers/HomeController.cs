bdb5f378d050c9938617976783cfd6c2b5acea0b
Dan Tatsenko <dan.tatsenko@propellerhead.co.nz>
2021-10-23 00:16:42 +1300

    version 1.0

--- PhoneticAlphabet/Controllers/HomeController.cs
+++ PhoneticAlphabet/Controllers/HomeController.cs
@@ -4,34 +4,23 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using PhoneticAlphabet.Models;
 
 namespace PhoneticAlphabet.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
+        [HttpGet]
+        public IActionResult Index(string message)
+        { 
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult Post(string message)
         {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var list = new MilitaryNotation().Get(message);
+            return View("Index",list);
         }
     }
 }
---
+++ PhoneticAlphabet/Models/MilitaryNotation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PhoneticAlphabet.Models
+{
+    public class MilitaryNotation
+    {
+        private Dictionary<char, string> abc;
+
+        public MilitaryNotation()
+        {
+            abc = new Dictionary<char, string>
+            {
+                { 'a', "Alfa" },
+                { 'b', "Bravo" },
+                { 'c', "Charlie" },
+                { 'd', "Delta" },
+                { 'e', "Echo" },
+                { 'f', "Foxtrot" },
+                { 'g', "Golf" },
+                { 'h', "Hotel" },
+                { 'i', "India" },
+                { 'j', "Juliet" },
+                { 'k', "Kilo" },
+                { 'l', "Lima" },
+                { 'm', "Mike" },
+                { 'n', "November" },
+                { 'o', "Oscar" },
+                { 'p', "Papa" },
+                { 'q', "Quebec" },
+                { 'r', "Romeo" },
+                { 's', "Sierra" },
+                { 't', "Tango" },
+                { 'u', "Uniform" },
+                { 'v', "Victor" },
+                { 'w', "Whiskey" },
+                { 'x', "X-ray" },
+                { 'y', "Yankee" },
+                { 'z', "Zulu" },
+            };
+            
+        }
+
+        public List<string> Get(string word)
+        {
+            List<string> result = new List<string>();
+            if (!string.IsNullOrEmpty(word))
+            {
+                foreach (var c in word.ToLower())
+                    result.Add(abc.GetValueOrDefault(c, string.Empty) + " for " + c.ToString().ToUpper());
+                return result;
+            }
+            else
+                return null;
+        }
+    }
+}
